6211b8552a35a83d45a55979d2855fa87cd4eaf6
Juan Camilo Acosta Miranda <juancamiloacosta521@gmail.com>
2021-10-13 23:55:38 -0500

    configuracion del repositorio

---
+++ src/main/java/net/software/backendcursojava/controllers/UserController.java
@@ -0,0 +1,45 @@
+package net.software.backendcursojava.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.software.backendcursojava.models.request.UserDetailRequestModel;
+import net.software.backendcursojava.models.responses.UserRest;
+import net.software.backendcursojava.services.UserServiceInterface;
+import net.software.backendcursojava.shared.dto.UserDTO;
+
+@RestController
+@RequestMapping("/users") //localhost/users
+public class UserController{
+
+    @Autowired
+    UserServiceInterface userService;
+    
+
+    @GetMapping //Obtener, consultar informacion
+    public String getUser(){
+        return "Obtener usuarios";
+    }
+
+    @PostMapping //Creando informacion
+    public UserRest createUser(@RequestBody UserDetailRequestModel userDetails){
+        UserRest UserToReturn= new UserRest();
+
+        UserDTO userDTO =new UserDTO();
+
+        BeanUtils.copyProperties(userDetails, userDTO);
+
+        UserDTO createdUser= userService.createUser(userDTO);
+
+        BeanUtils.copyProperties(createdUser, UserToReturn);
+
+        return UserToReturn;
+    }
+
+}
---
+++ src/main/java/net/software/backendcursojava/services/UserService.java
@@ -0,0 +1,55 @@
+package net.software.backendcursojava.services;
+
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import net.software.backendcursojava.UserRepository;
+import net.software.backendcursojava.entities.UserEntity;
+import net.software.backendcursojava.shared.dto.UserDTO;
+
+@Service
+public class UserService implements UserServiceInterface {
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDTO createUser(UserDTO userDto) {
+
+        if(userRepository.findUserByEmail(userDto.getEmail()) != null) 
+            throw new RuntimeException("El email ya existe");
+        
+        // TODO logica de la creacion del usuario
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDto, userEntity);
+
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+        
+        UUID userId = UUID.randomUUID();
+        userEntity.setUserId(userId.toString());
+       
+
+        UserEntity storedUserDetail =  userRepository.save(userEntity);
+
+        UserDTO userToReturn = new UserDTO();
+        BeanUtils.copyProperties(storedUserDetail, userToReturn);
+        return userToReturn;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
